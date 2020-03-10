506a7812317ba108eaeec2180113a9faa3ac5d84
tc652184 <4f02c6cbcb91fed7f7788dd3b52bf3ff10ef56a9>
2020-03-10 21:43:49 +0100

    compare

--- com.ca.lsp.cobol/lsp-service-cobol/src/main/java/com/ca/lsp/cobol/service/TextDocumentSyncType.java
+++
@@ -1,7 +0,0 @@
-package com.ca.lsp.cobol.service;
-
-/** This enum class used to track text synchronization type for the processed document. */
-public enum TextDocumentSyncType {
-  DID_OPEN,
-  DID_CHANGE
-}
